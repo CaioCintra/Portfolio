0af5bca53258eb4978b6e117512cd352a475f922
CaioCintra <caiocintra2000@gmail.com>
2024-04-16 17:33:09 -0300

    Add Pokedex

--- src/app/components/Projects.tsx
+++ src/app/components/Projects.tsx
@@ -6,8 +6,25 @@ export default function Projects() {
       <p className="text-3xl font-bold text-center m-11">Projetos</p>
 
       <div className="overflow-y-auto align-middle flex items-center flex-wrap justify-center">
-        <Project img="/SistemaTCC.png" link="https://github.com/CaioCintra/Sistema_TCC" nome="Sistema Gerenciador de TCC" desc="Sistema para simplificar o trabalho do professor responsável pelas atividades de trabalho de conclusão de curso da UTFPR."/>
-        <Project img="/Capsula.png" link="https://github.com/CaioCintra/CapsulaDoTempo" nome="Capsula Do Tempo" desc="Armazém de lembranças com datas, autenticado pelo github, feito com ajuda do curso da Rocketseat"/>
+        <Project
+          img="/SistemaTCC.png"
+          link="https://github.com/CaioCintra/Sistema_TCC"
+          nome="Sistema Gerenciador de TCC"
+          desc="Sistema para simplificar o trabalho do professor responsável pelas atividades de trabalho de conclusão de curso da UTFPR."
+        />
+        <Project
+          img="/Pokedex.png"
+          link="https://github.com/CaioCintra/Pokedex"
+          site="https://pokedex-caiocintra.vercel.app"
+          nome="Pokédex"
+          desc="Pokédex interativa, feita para treinar em Next.js"
+        />
+        <Project
+          img="/Capsula.png"
+          link="https://github.com/CaioCintra/CapsulaDoTempo"
+          nome="Cápsula Do Tempo"
+          desc="Armazém de lembranças com datas, autenticado pelo github, feito com ajuda do curso da Rocketseat"
+        />
       </div>
     </>
   );
