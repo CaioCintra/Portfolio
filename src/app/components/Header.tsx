e4406a8da007a11f152d174a53120438de91ea65
CaioCintra <caiocintra2000@gmail.com>
2024-04-26 22:59:58 -0300

    Z index error fix

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,7 +1,7 @@
 export default function Header() {
     return (
       <header>
-        <div className="w-full h-20 bg-violet-950 flex items-center flex-wrap justify-around fixed top-0 shadow-black shadow-sm">
+        <div className="w-full h-20 z-50 bg-violet-950 flex items-center flex-wrap justify-around fixed top-0 shadow-black shadow-sm">
           <p className="font-bold text-3xl text-zinc-200 hover:text-zinc-400 ease-in-out duration-500">Portfólio</p>
         </div>
       </header>
